c2dce6c50f5acb98dd403f216d06a0ca4605b6ec
Alexandre Ballesté <alex.balleste@gmail.com>
2016-10-07 17:33:47 +0200

    Update tree-plugin-underscore.js

--- tree-plugin-underscore.js
+++ tree-plugin-underscore.js
@@ -1,28 +1,25 @@
 (function() {
 
-      var matchAttribute = function (dataObject, key, value) {
-        return value === _.property(keu)(dataObject);
-      };
-      
-      var buildTree  = function(flatArray, elementId, parentElementId, childrenCollectionName, rootElementId) {
-        elementId  = elementId || 'id';
-        parentElementId = parentElementId || 'parentId';
-	childrenCollectionName = childrenCollectionName || 'children';
-        rootElementId = rootElementId || (_.first(flatArray))[elementId] || 0;
-       
-        var output = _.clone(_.find(flatArray, _.partial(matchAttribute, elementId, rootElementId))),
-            children = _.filter(flatArray, _.partial(matchAttribute, parentelementId, rootElementId));
-       
+  var matchAttribute = function  (key, value, dataObject) {
+    return (value === _.property(key)(dataObject));
+  };
 
-        output[childrenCollectionName] = _.map(children, function(child) {
-          return buildTree (flatArray, elementId, parentElementId, childrenCollectionName, child[elementId]);
-        });
-       
-        return output;
-      };
+  var buildTree  = function(flatArray, rootIdValue, idName, parentIdName, childrenCollectionName) {
+    idName  = idName || 'id';
+    parentIdName = parentIdName || 'parentId';
+    childrenCollectionName = childrenCollectionName || 'children';
+    rootIdValue = rootIdValue || (_.first(flatArray))[idName] || 0;
+
+    var output = _.clone(_.find(flatArray, _.partial(matchAttribute, idName, rootIdValue)));
+    var children = _.filter(flatArray, _.partial(matchAttribute, parentIdName, rootIdValue));
+
+    output[childrenCollectionName] = _.map(children, function(child) {
+      return buildTree (flatArray, child[idName], idName, parentIdName, childrenCollectionName);
+    });
+
+    return output;
+  };
+
+  _.mixin ({tree: buildTree});
   
-  _.mixin({
-    tree: buildTree
-  });
-
 }).call(this);
